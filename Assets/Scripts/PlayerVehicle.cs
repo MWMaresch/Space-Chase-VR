8400eed206a0df7d37e7a0fcd6f042dfd4f655ed
MWMaresch <michael@maresch.co>
2016-10-06 20:03:56 -0400

    Added inital vehicle controls with a testing scene
    The controls right now are very slippery and completely require a
    controller to use effectively. There are bindings for keyboard+mouse,
    but they're not very good at the moment. The script that's there serves
    more as a base for testing and improvement than it does anything final.
    Everything should currently natively work with an XBox 360 controller.

---
+++ Assets/Scripts/PlayerVehicle.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerVehicle : MonoBehaviour {
+
+    public float maxSpeed = 1; //when accelerating, we cannot go past this speed
+    public float acceleration = 0.01f; //how much speed increases when we accelerate
+    public float deceleration = 1.01f; //rate at which vehicle slows down when not accelerating
+    public float turnStrength = 0.1f; //the higher this is, the tighter the turn
+    public float turnFriction = 0.07f; //how much we slow down when turning
+    public float turnDeceleration = 0.9f; //how much the turn slows down
+    public float maxTurnSpeed = 0; //we cannot turn faster than this
+    public float strafeStrength = 0.01f; //how much we move to the side when holding a strafe button
+    public float brakeStrength = 1.01f; //how much we slow down when braking
+
+    private Rigidbody _rigidbody;
+    private Vector3 _curVelocity;
+    private Vector3 _curRotation;
+    private Vector3 _curStrafe;
+
+    //public float brakeGripLoss; //how much we slide when braking
+    //grip not implemented, may not be necessary at all to implement
+
+	// Use this for initialization
+	void Start () {
+        _curVelocity = new Vector3(0, 0, 0);
+        _curRotation = new Vector3(0, 0, 0);
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        UpdateControls();
+        transform.position += _curVelocity;
+        //
+    }
+
+    private void UpdateControls()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        //turn
+        _curVelocity /= (Mathf.Abs(h * turnFriction * (turnStrength/10)) + 1);
+        _curVelocity /= (Mathf.Abs(v * turnFriction * (turnStrength / 10)) + 1);
+        //h is between -1 and 1, so we make it between 1 and 2,
+        //where 1 represents 0 and 2 represents both 1 and -1
+        //to avoid division by 0 while still being able to use these variables
+        
+        _curRotation += new Vector3(v * turnStrength, h * turnStrength, 0);
+        _curRotation *= turnDeceleration;
+        
+        transform.localEulerAngles += _curRotation;
+
+        //accelerate
+        if (Input.GetButton("Accelerate"))
+        {
+            //instead of refusing to add more speed when we're at the max,
+            //we clamp it after adding more to allow velocity regarding turning to exist
+            _curVelocity += (transform.forward * acceleration);
+            _curVelocity = Vector3.ClampMagnitude(_curVelocity, maxSpeed);
+        }
+        else
+        {
+            _curVelocity /= deceleration;
+        }
+
+        //brake
+        if (Input.GetButton("Brake"))
+        {
+            _curVelocity /= brakeStrength;
+        }
+
+        //strafe
+        if (Input.GetButton("StrafeLeft"))
+        {
+            _curVelocity += (-transform.right * strafeStrength);
+        }
+        if (Input.GetButton("StrafeRight"))
+        {
+            _curVelocity += (transform.right * strafeStrength);
+        }
+        //insert code to make the vehicle lean left/right when strafing
+    }
+}
